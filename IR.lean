110152128305f0c386ec0df0d95a11603c257e97
Alexandre Rademaker <arademaker@gmail.com>
2024-08-13 13:41:31 -0300

    IR: chapter 1

--- IR.lean
+++ IR.lean
@@ -1,1 +1,2 @@
 import «IR».Basic
+import «IR».Chapter1
--- Main.lean
+++ Main.lean
@@ -1,8 +1,11 @@
 import «CS»
 import «IR»
 
+open IR.Chapter1
+
 set_option maxRecDepth 100000 in
 #time def test := Chapter1.words₁ Chapter1.sonnet18
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def main : IO Unit := do
+  let ls ← dhbb (Query.and (Query.w "senadores") (Query.w "votos"))
+  IO.println ls
