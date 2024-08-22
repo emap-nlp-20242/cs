1341d893e7191007b900daf0fc416041e0ee274c
Alexandre Rademaker <arademaker@gmail.com>
2024-08-21 21:37:50 -0300

    codigo inicial cap 3

--- IR.lean
+++ IR.lean
@@ -1,2 +1,3 @@
 import «IR».Basic
 import «IR».Chapter1
+import «IR».Chapter3
---
+++ IR/Chapter3.lean
@@ -0,0 +1,23 @@
+
+namespace Chapter3
+
+def rotations (s : String) : List String :=
+ List.map (λ n => s.drop n ++ s.take n) (List.range s.length)
+
+#eval rotations "recuperação$"
+
+def tails : List a → List (List a)
+| [] => [[]]
+| m@(_ :: xs) => m :: tails xs
+
+def grams (n : Nat) (s : String) : List String :=
+ if n > s.length then
+  []
+ else
+  List.filter (fun x => x.length == n) ∘ List.map (fun x => x.take n)
+   $ (tails s.toList) |>.map (fun x => x.asString)
+
+#eval grams 3 "recuperação"
+
+
+end Chapter3
